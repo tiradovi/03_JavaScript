152299c1118e756c8a4317f7384fa38ba7e5084e
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-07-25 14:22:34 +0900

    회원가입 비밀번호 확인

---
+++ js/09_이벤트_회원가입_실습.js
@@ -0,0 +1,23 @@
+const password = document.getElementById("password");
+const passwordConfirm = document.getElementById("password-confirm");
+const passwordResult = document.getElementById("password-result");
+
+passwordConfirm.addEventListener("keyup", function (e) {
+  if (e.key == "Enter") {
+    if (password.value.trim() || passwordConfirm.value.trim) {
+      passwordResult.textContent = `비밀번호를 입력하세요`;
+    } else if (password.value == passwordConfirm.value) {
+      passwordResult.textContent = `비밀번호가 일치합니다!
+                                 원본비밀번호 : ${password.value}                             
+                                 확인비밀번호 :${passwordConfirm.value}                           
+                                 원본비밀번호 길이 :${password.value.length}
+                                 확인비밀번호 길이 :${passwordConfirm.value.length}
+                                 `;
+    } else {
+      passwordResult.textContent = `
+                                 원본비밀번호 : ${password.value}                             
+                                 확인비밀번호 :${passwordConfirm.value}
+                                 `;
+    }
+  }
+});
