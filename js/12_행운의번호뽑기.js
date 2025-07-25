41ba4b29817e780df31333753e60d6310aa70265
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-07-25 17:01:12 +0900

    행운의 로또 번호 뽑기 실습

---
+++ js/12_행운의번호뽑기.js
@@ -0,0 +1,18 @@
+const lottoResult = document.getElementById("lottoResult");
+const lottoBtn = document.getElementById("lottobutton");
+
+lottoBtn.addEventListener("click", function () {
+  let result = "";
+  for (let i = 0; i < 6; i++) {
+    const num = Math.floor(Math.random() * 45) + 1;
+
+    if (i === 0) {
+      result = num;
+    } else if (i === 5) {
+      result += " + " + num;
+    } else {
+      result += ", " + num;
+    }
+  }
+  lottoResult.innerText = result;
+});
