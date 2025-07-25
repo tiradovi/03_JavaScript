40055b7c53dca250cb3d62d8b198a8f4bb2b8630
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-07-25 12:40:59 +0900

    이벤트(input, change, submit) 실습

--- js/08_이벤트_event.js
+++ js/08_이벤트_event.js
@@ -37,3 +37,43 @@ btnC.addEventListener("click", function (e) {
                          ID 명칭:${e.target.id} 
                          Class 명칭: ${e.target.className}`;
 });
+
+const textInput = document.getElementById("text-input");
+const numberInput = document.getElementById("number-input");
+const selectInput = document.getElementById("select-input");
+const result3 = document.getElementById("result3");
+
+// 텍스트 입력이 감지되었을 때 진행할 기능 설정
+textInput.addEventListener("input", function (e) {
+  result3.textContent = `텍스트 입력값 : ${e.target.value}
+                         텍스트 글자수 : ${e.target.value.length}`;
+});
+// 숫자 입력이 감지되었을 때 숫자입력 값과 글자수 result3로 보이기
+numberInput.addEventListener("input", function (e) {
+  result3.textContent = `텍스트 입력값 : ${e.target.value}
+                         텍스트 글자수 : ${e.target.value.length}`;
+});
+// select에서 선택한 옵션을 클릭해서 변경할 때 변경된 값 보여주기
+selectInput.addEventListener("change", function (e) {
+  result3.textContent = `선택된 값: ${e.target.value}
+                         선택된 값의 글자수: ${e.target.value.length}
+                         선택된 값의 순서: ${e.target.selectedIndex}`;
+});
+
+const loginForm = document.getElementById("login-form");
+const username = document.getElementById("username");
+const userPw = document.getElementById("userPw");
+const result4 = document.getElementById("result4");
+
+loginForm.addEventListener("submit", function (e) {
+  //preventDefault 뒤에 () 붙이기
+  /* 
+  함수(기능, 행동) 내부에 메서드(기능,행동)
+ 
+  변수이름.메서드행동기능함수명칭()=동사
+  변수이름.명칭                  =명사
+  */
+  e.preventDefault(); // 실제 폼 제출을 우선 멈춤 , 비밀번호 일치 확인, 아이디 중복 확인 등의 기능에 사용
+  result4.textContent = `사용자 이  름: ${username.value}
+                        사용자 비밀번호: ${userPw.value}`;
+});
